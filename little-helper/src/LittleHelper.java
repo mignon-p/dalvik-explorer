65c836375287ce9a6780969fb875f7fcfdf9c781
enh@jessies.org <enh@jessies.org@825dafb7-1f56-0410-864d-61665d7775f4>
2008-09-24 04:53:57 +0000

    little-helper/src/LittleHelper.java: support for plain old URLs and weather forecasts.
    git-svn-id: http://enh.googlecode.com/svn/trunk@75 825dafb7-1f56-0410-864d-61665d7775f4

--- little-helper/src/LittleHelper.java
+++ little-helper/src/LittleHelper.java
@@ -2,9 +2,14 @@ import e.gui.*;
 import e.util.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.*;
 import java.util.*;
 import java.util.List;
 import java.util.regex.*;
+import javax.imageio.*;
 import javax.swing.*;
 import org.jessies.test.*;
 
@@ -71,6 +76,7 @@ public class LittleHelper extends JFrame {
         ui.add(textField, BorderLayout.NORTH);
         
         this.resultList = new JList();
+        resultList.setCellRenderer(new CustomListCellRenderer());
         resultList.setVisibleRowCount(8);
         ui.add(new JScrollPane(resultList), BorderLayout.CENTER);
         
@@ -86,22 +92,25 @@ public class LittleHelper extends JFrame {
         
         // FIXME: do everything from this point down on another thread.
         
+        // Sometimes a URL is just a URL...
+        if (query.startsWith("http://")) {
+            openUrl(query);
+        }
+        
         // Do we have a "verb"?
         for (Verb verb : verbs) {
             final String url = verb.getUrl(query);
             if (url != null) {
-                try {
-                    BrowserLauncher.openURL(url);
-                } catch (java.io.IOException ex) {
-                    // bark
-                }
-                // Our work here is done.
-                // FIXME: this is a bit harsh (but effective: MainFrame doesn't seem to work right, and even when it does it's slow).
-                System.exit(0);
+                openUrl(url);
             }
         }
         
         DefaultListModel model = new DefaultListModel();
+        
+        // Weather is popular...
+        if (query.equals("weather")) {
+            showWeatherForecast(model);
+        }
         
         // Convert bases.
         NumberDecoder numberDecoder = new NumberDecoder(query);
@@ -120,6 +129,19 @@ public class LittleHelper extends JFrame {
         // FIXME: Calculator.
         
         resultList.setModel(model);
+    }
+    
+    private void openUrl(String url) {
+        try {
+            System.err.println(url);
+            BrowserLauncher.openURL(url);
+            // Our work here is done.
+            // FIXME: this is a bit harsh (but effective: MainFrame doesn't seem to work right, and even when it does it's slow).
+            System.exit(0);
+        } catch (java.io.IOException ex) {
+            ex.printStackTrace();
+            // bark
+        }
     }
     
     @Test private static void testTemperatureConversion() {
@@ -244,6 +266,118 @@ public class LittleHelper extends JFrame {
         verbs.add(new Verb("^(?:i(?:mdb)?) +(.*)", "http://www.imdb.com/find?s=all&q=%s&x=0&y=0"));
         verbs.add(new Verb("^(?:p(?:osix)?) +(.*)", "http://opengroup.org/cgi-bin/kman2?value=%s"));
         verbs.add(new Verb("^(?:w(?:ikipedia)?) +(.*)", "http://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go"));
+    }
+    
+    private static void showWeatherForecast(DefaultListModel model) {
+        // "http://www.bbc.co.uk/weather/5day.shtml?world=4079" // Basel, CH.
+        // "http://www.bbc.co.uk/weather/5day.shtml?world=0306" // San Jose, CA, US.
+        final String location = "http://www.bbc.co.uk/weather/5day.shtml?world=0306";
+        final ArrayList<DayForecast> forecast = collectForecastFor(location);
+        for (DayForecast day : forecast) {
+            model.addElement(day);
+        }
+    }
+    
+    private static ArrayList<DayForecast> collectForecastFor(String location) {
+        ArrayList<DayForecast> forecast = new ArrayList<DayForecast>();
+        
+        // FIXME: cache the forecast so we don't re-fetch it more than, say, once per hour.
+        String content;
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(new URL(location).openStream()));
+            StringBuilder buffer = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                buffer.append(line);
+                buffer.append('\n');
+            }
+            content = buffer.toString();
+        } catch (IOException ex) {
+            Log.warn("Failed to read forecast.", ex);
+            return null;
+        } finally {
+            FileUtilities.close(in);
+        }
+        
+        Pattern cityNamePattern = Pattern.compile("^\\s+<title>BBC.* Forecast in .* for (.+),");
+        Pattern dayNamePattern = Pattern.compile("class=\"weatherday\".*strong>(\\S+)<br");
+        Pattern imageUrlPattern = Pattern.compile("img src=\"(\\S+\\/fiveday_sym\\/\\d+\\S+)\"");
+        Pattern dayTemperaturePattern = Pattern.compile("class=\"temptxt\"><strong>(\\d+)<.*Day ");
+        Pattern nightTemperaturePattern = Pattern.compile("class=\"temptxt\">.*<strong>(\\d+)<.*Night ");
+        String dayName = null;
+        String imageUrl = null;
+        int dayTemperatureC = 0;
+        Matcher matcher;
+        for (String line : content.split("\n")) {
+            if ((matcher = cityNamePattern.matcher(line)).find()) {
+                // FIXME: make some use of the location name.
+                //setTitle(matcher.group(1) + " 5 Day Forecast");
+            } else if ((matcher = dayNamePattern.matcher(line)).find()) {
+                dayName = matcher.group(1);
+            } else if ((matcher = imageUrlPattern.matcher(line)).find()) {
+                imageUrl = "http://www.bbc.co.uk" + matcher.group(1);
+            } else if ((matcher = dayTemperaturePattern.matcher(line)).find()) {
+                dayTemperatureC = Integer.parseInt(matcher.group(1));
+            } else if ((matcher = nightTemperaturePattern.matcher(line)).find()) {
+                int nightTemperatureC = Integer.parseInt(matcher.group(1));
+                DayForecast dayForecast = new DayForecast(dayName, imageUrl, dayTemperatureC, nightTemperatureC);
+                forecast.add(dayForecast);
+            }
+        }
+        
+        if (forecast.size() != 5) {
+            System.out.println(content);
+            for (DayForecast dayForecast : forecast) {
+                System.out.println(dayForecast);
+            }
+        }
+        
+        return forecast;
+    }
+    
+    public static class CustomListCellRenderer extends DefaultListCellRenderer {
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            if (value instanceof DayForecast) {
+                ((DayForecast) value).configureCellRenderer(this, list, isSelected);
+                setEnabled(list.isEnabled());
+            }
+            return this;
+        }
+    }
+    
+    private static class DayForecast {
+        private final String day;
+        private ImageIcon icon;
+        private final int dayTemperatureC;
+        private final int nightTemperatureC;
+        
+        public DayForecast(String day, String imageUrl, int dayTemperatureC, int nightTemperatureC) {
+            this.day = day;
+            this.dayTemperatureC = dayTemperatureC;
+            this.nightTemperatureC = nightTemperatureC;
+            
+            // FIXME: doesn't GNOME have suitable icons already available?
+            // FIXME: we should at least cache these on local disk!
+            try {
+                BufferedImage image = ImageIO.read(new URL(imageUrl));
+                BufferedImage scaledImage = new AffineTransformOp(AffineTransform.getScaleInstance(0.5, 0.5), AffineTransformOp.TYPE_BILINEAR).filter(image, null);
+                this.icon = new ImageIcon(scaledImage);
+            } catch (IOException ex) {
+                Log.warn("Failed to read image \"" + imageUrl + "\".", ex);
+            }
+        }
+        
+        private void configureCellRenderer(JLabel renderer, JList list, boolean isSelected) {
+            final String DEGREES_C = "\u00b0C";
+            renderer.setIcon(icon);
+            renderer.setText(day + " " + dayTemperatureC + DEGREES_C + " (" + nightTemperatureC + DEGREES_C + ")");
+        }
+        
+        public String toString() {
+            return "DayForecast[day=" + day + ",icon=" + icon + ",dayTemperatureC=" + dayTemperatureC + ",nightTemperatureC=" + nightTemperatureC + "]";
+        }
     }
     
     public static void main(String[] args) {
