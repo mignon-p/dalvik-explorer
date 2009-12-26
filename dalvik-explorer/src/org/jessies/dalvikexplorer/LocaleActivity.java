63f3852b2780949c65937b5ac1d5b7dc1c8dc477
enh@jessies.org <enh@jessies.org@825dafb7-1f56-0410-864d-61665d7775f4>
2009-12-26 07:11:09 +0000

    dalvik-explorer/src/org/jessies/dalvikexplorer/LocaleActivity.java: show details of date/time formatting.
    git-svn-id: http://enh.googlecode.com/svn/trunk@357 825dafb7-1f56-0410-864d-61665d7775f4

--- dalvik-explorer/src/org/jessies/dalvikexplorer/LocaleActivity.java
+++ dalvik-explorer/src/org/jessies/dalvikexplorer/LocaleActivity.java
@@ -5,6 +5,7 @@ import android.content.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import java.text.*;
 import java.util.*;
 
 public class LocaleActivity extends Activity {
@@ -67,7 +68,45 @@ public class LocaleActivity extends Activity {
             result.append("Display Variant: " + locale.getDisplayVariant() + "\n");
             result.append("Variant Code: " + locale.getVariant() + "\n");
         }
+        result.append('\n');
+        
+        Date now = new Date(); // FIXME: it might be more useful to always show a time in the afternoon, to make 24-hour patterns more obvious.
+        result.append("Date/Time Patterns\n\n");
+        describeDateFormat(result, "Full Date", DateFormat.getDateInstance(DateFormat.FULL, locale), now);
+        describeDateFormat(result, "Medium Date", DateFormat.getDateInstance(DateFormat.MEDIUM, locale), now);
+        describeDateFormat(result, "Short Date", DateFormat.getDateInstance(DateFormat.SHORT, locale), now);
+        result.append('\n');
+        describeDateFormat(result, "Full Time", DateFormat.getTimeInstance(DateFormat.FULL, locale), now);
+        describeDateFormat(result, "Medium Time", DateFormat.getTimeInstance(DateFormat.MEDIUM, locale), now);
+        describeDateFormat(result, "Short Time", DateFormat.getTimeInstance(DateFormat.SHORT, locale), now);
+        result.append('\n');
+        describeDateFormat(result, "Full Date/Time", DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, locale), now);
+        describeDateFormat(result, "Medium Date/Time", DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM, locale), now);
+        describeDateFormat(result, "Short Date/Time", DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, locale), now);
+        result.append('\n');
+        result.append('\n');
+        
+        result.append("Date Format Symbols\n\n");
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, locale);
+        if (df instanceof SimpleDateFormat) {
+            DateFormatSymbols dfs = ((SimpleDateFormat) df).getDateFormatSymbols();
+            result.append("Am/pm: " + Arrays.toString(dfs.getAmPmStrings()) + "\n");
+            result.append("Eras: " + Arrays.toString(dfs.getEras()) + "\n");
+            result.append("Local Pattern Chars: " + dfs.getLocalPatternChars() + "\n");
+            result.append("Months: " + Arrays.toString(dfs.getMonths()) + "\n");
+            result.append("Short Months: " + Arrays.toString(dfs.getShortMonths()) + "\n");
+            result.append("Weekdays: " + Arrays.toString(dfs.getWeekdays()) + "\n");
+            result.append("Short Weekdays: " + Arrays.toString(dfs.getShortWeekdays()) + "\n");
+        }
         
         return result.toString();
+    }
+    
+    private static void describeDateFormat(StringBuilder result, String description, DateFormat dateFormat, Date when) {
+        if (dateFormat instanceof SimpleDateFormat) {
+            SimpleDateFormat sdf = (SimpleDateFormat) dateFormat;
+            result.append(description + ": " + sdf.toLocalizedPattern() + "\n");
+            result.append("    " + sdf.format(when) + "\n");
+        }
     }
 }
