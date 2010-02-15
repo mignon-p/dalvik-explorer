0679da1d92acb609e6a981a5626cf9054124d70e
enh@jessies.org <enh@jessies.org@825dafb7-1f56-0410-864d-61665d7775f4>
2010-02-15 04:29:17 +0000

    dalvik-explorer/src/org/jessies/dalvikexplorer/TextViewActivity.java: give each TextView-based activity a context menu with "copy" and "send as mail" options.
    git-svn-id: http://enh.googlecode.com/svn/trunk@402 825dafb7-1f56-0410-864d-61665d7775f4

--- dalvik-explorer/src/org/jessies/dalvikexplorer/TextViewActivity.java
+++ dalvik-explorer/src/org/jessies/dalvikexplorer/TextViewActivity.java
@@ -1,13 +1,19 @@
 package org.jessies.dalvikexplorer;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.text.ClipboardManager;
+import android.view.*;
 import android.widget.*;
 
 /**
  * An abstract superclass for our TextView-based activities.
  */
 public abstract class TextViewActivity extends Activity {
+    private static final int CONTEXT_MENU_COPY = 0;
+    private static final int CONTEXT_MENU_MAIL = 1;
+    
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
@@ -18,9 +24,11 @@ public abstract class TextViewActivity extends Activity {
             extraValue = getIntent().getStringExtra(extraName);
         }
         
-        TextView textView = (TextView) findViewById(R.id.output);
+        final TextView textView = (TextView) findViewById(R.id.output);
         textView.setText(content(extraValue));
         setTitle(title(extraValue));
+        
+        registerForContextMenu(textView);
     }
     
     protected String extraName() {
@@ -30,4 +38,40 @@ public abstract class TextViewActivity extends Activity {
     protected abstract CharSequence title(String extraValue);
     
     protected abstract CharSequence content(String extraValue);
+    
+    @Override public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Details");
+        menu.add(0, CONTEXT_MENU_COPY,  0, "Copy");
+        menu.add(0, CONTEXT_MENU_MAIL,  0, "Send as mail");
+    }
+    
+    @Override public boolean onContextItemSelected(MenuItem item) {
+        final TextView textView = (TextView) findViewById(R.id.output);
+        final CharSequence title = getTitle();
+        final CharSequence content = textView.getText();
+        switch (item.getItemId()) {
+        case CONTEXT_MENU_COPY:
+            return copyToClipboard(title, content);
+        case CONTEXT_MENU_MAIL:
+            return mail(title, content);
+        default:
+            return super.onContextItemSelected(item);
+        }
+    }
+    
+    private boolean copyToClipboard(CharSequence title, CharSequence content) {
+        final ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        clipboard.setText(title + "\n\n" + content);
+        return true;
+    }
+    
+    private boolean mail(CharSequence title, CharSequence content) {
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Dalvik Explorer: " + title);
+        intent.putExtra(Intent.EXTRA_TEXT, content);
+        startActivity(intent);
+        return true;
+    }
 }
