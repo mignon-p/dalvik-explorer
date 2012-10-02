c7c63af818cacae4af5422ecfb5c6bf5dd922678
elliott.n.hughes@gmail.com <elliott.n.hughes@gmail.com@825dafb7-1f56-0410-864d-61665d7775f4>
2012-10-02 23:36:19 +0000

    src/org/jessies/dalvikexplorer/LocaleActivity.java: only show stand-alone names if they're different from the regular names.
    git-svn-id: http://enh.googlecode.com/svn/trunk@608 825dafb7-1f56-0410-864d-61665d7775f4

--- dalvik-explorer/src/org/jessies/dalvikexplorer/LocaleActivity.java
+++ dalvik-explorer/src/org/jessies/dalvikexplorer/LocaleActivity.java
@@ -12,48 +12,48 @@ public class LocaleActivity extends TextViewActivity {
   protected String extraName() {
     return "org.jessies.dalvikexplorer.Locale";
   }
-  
+
   protected CharSequence title(String localeName) {
     return "Locale \"" + localeName + "\"";
   }
-  
+
   protected CharSequence content(String localeName) {
     return describeLocale(localeName);
   }
-  
+
   private static Locale localeByName(String name) {
     if (name.isEmpty()) {
       return new Locale("", "", "");
     }
-    
+
     int languageEnd = name.indexOf('_');
     if (languageEnd == -1) {
       return new Locale(name, "", "");
     }
-    
+
     String language = name.substring(0, languageEnd);
     name = name.substring(languageEnd + 1);
-    
+
     int countryEnd = name.indexOf('_');
     if (countryEnd == -1) {
       return new Locale(language, name, "");
     }
-    
+
     String country = name.substring(0, countryEnd);
     String variant = name.substring(countryEnd + 1);
-    
+
     return new Locale(language, country, variant);
   }
-    
+
   static String describeLocale(String name) {
     final StringBuilder result = new StringBuilder();
-    
+
     final Locale locale = localeByName(name);
-    
+
     result.append("Display Name: " + locale.getDisplayName() + "\n");
     result.append("Localized Display Name: " + locale.getDisplayName(locale) + "\n");
     result.append('\n');
-    
+
     if (locale.getLanguage().length() > 0) {
       result.append("Display Language: " + locale.getDisplayLanguage() + "\n");
       result.append("Localized Display Language: " + locale.getDisplayLanguage(locale) + "\n");
@@ -74,7 +74,7 @@ public class LocaleActivity extends TextViewActivity {
       result.append("Variant Code: " + locale.getVariant() + "\n");
     }
     result.append('\n');
-    
+
     result.append("Number Formatting\n\n");
     describeNumberFormat(result, "Decimal", NumberFormat.getInstance(locale), 1234.5, -1234.5);
     describeNumberFormat(result, "Integer", NumberFormat.getIntegerInstance(locale), 1234, -1234);
@@ -82,9 +82,9 @@ public class LocaleActivity extends TextViewActivity {
     describeNumberFormat(result, "Percent", NumberFormat.getPercentInstance(locale), 12.3);
     result.append('\n');
     result.append('\n');
-    
+
     boolean hasLocaleData = hasLocaleData();
-    
+
     if (!hasLocaleData) {
       result.append("Decimal Format Symbols\n\n");
       NumberFormat nf = NumberFormat.getInstance(locale);
@@ -94,7 +94,7 @@ public class LocaleActivity extends TextViewActivity {
         result.append("(Didn't expect " + nf.getClass() + ".)");
       }
     }
-    
+
     Date now = new Date(); // FIXME: it might be more useful to always show a time in the afternoon, to make 24-hour patterns more obvious.
     result.append("Date/Time Formatting\n\n");
     describeDateFormat(result, "Full Date", DateFormat.getDateInstance(DateFormat.FULL, locale), now);
@@ -113,7 +113,7 @@ public class LocaleActivity extends TextViewActivity {
     describeDateFormat(result, "Short Date/Time", DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, locale), now);
     result.append('\n');
     result.append('\n');
-    
+
     if (!hasLocaleData) {
       result.append("Date Format Symbols\n\n");
       DateFormat edf = DateFormat.getDateInstance(DateFormat.FULL, Locale.US);
@@ -130,7 +130,7 @@ public class LocaleActivity extends TextViewActivity {
       result.append('\n');
       result.append('\n');
     }
-    
+
     result.append("Calendar\n\n");
     Calendar c = Calendar.getInstance(locale);
     int firstDayOfWeek = c.getFirstDayOfWeek();
@@ -144,7 +144,7 @@ public class LocaleActivity extends TextViewActivity {
     result.append("Minimal Days in First Week: " + c.getMinimalDaysInFirstWeek() + "\n");
     result.append('\n');
     result.append('\n');
-    
+
     // If this locale specifies a country, check out the currency.
     // Languages don't have currencies; countries do.
     if (!locale.getCountry().equals("")) {
@@ -156,7 +156,7 @@ public class LocaleActivity extends TextViewActivity {
       result.append('\n');
       result.append('\n');
     }
-    
+
     result.append("Data Availability\n\n");
     appendAvailability(result, locale, "BreakIterator", BreakIterator.class);
     appendAvailability(result, locale, "Calendar", NumberFormat.class);
@@ -165,39 +165,40 @@ public class LocaleActivity extends TextViewActivity {
     appendAvailability(result, locale, "DateFormatSymbols", DateFormatSymbols.class);
     appendAvailability(result, locale, "DecimalFormatSymbols", DecimalFormatSymbols.class);
     appendAvailability(result, locale, "NumberFormat", NumberFormat.class);
-    
+
     if (hasLocaleData) {
       result.append("\n\n");
       result.append("libcore.icu.LocaleData\n\n");
       try {
         Object enUsData = getLocaleDataInstance(Locale.US);
         Object localeData = getLocaleDataInstance(locale);
-        
-        describeStringArray(result, "amPm", enUsData, localeData);
-        describeStringArray(result, "eras", enUsData, localeData);
+
+        describeStringArray(result, "amPm", enUsData, localeData, null);
+        describeStringArray(result, "eras", enUsData, localeData, null);
         result.append('\n');
-        
-        describeStringArray(result, "longMonthNames", enUsData, localeData);
-        describeStringArray(result, "longStandAloneMonthNames", enUsData, localeData);
-        describeStringArray(result, "shortMonthNames", enUsData, localeData);
-        describeStringArray(result, "shortStandAloneMonthNames", enUsData, localeData);
-        describeStringArray(result, "tinyMonthNames", enUsData, localeData);
-        describeStringArray(result, "tinyStandAloneMonthNames", enUsData, localeData);
+
+        String[] previous;
+        previous = describeStringArray(result, "longMonthNames", enUsData, localeData, null);
+        describeStringArray(result, "longStandAloneMonthNames", enUsData, localeData, previous);
+        previous = describeStringArray(result, "shortMonthNames", enUsData, localeData, null);
+        describeStringArray(result, "shortStandAloneMonthNames", enUsData, localeData, previous);
+        previous = describeStringArray(result, "tinyMonthNames", enUsData, localeData, null);
+        describeStringArray(result, "tinyStandAloneMonthNames", enUsData, localeData, previous);
         result.append('\n');
-        
-        describeStringArray(result, "longWeekdayNames", enUsData, localeData);
-        describeStringArray(result, "longStandAloneWeekdayNames", enUsData, localeData);
-        describeStringArray(result, "shortWeekdayNames", enUsData, localeData);
-        describeStringArray(result, "shortStandAloneWeekdayNames", enUsData, localeData);
-        describeStringArray(result, "tinyWeekdayNames", enUsData, localeData);
-        describeStringArray(result, "tinyStandAloneWeekdayNames", enUsData, localeData);
+
+        previous = describeStringArray(result, "longWeekdayNames", enUsData, localeData, null);
+        describeStringArray(result, "longStandAloneWeekdayNames", enUsData, localeData, previous);
+        previous = describeStringArray(result, "shortWeekdayNames", enUsData, localeData, null);
+        describeStringArray(result, "shortStandAloneWeekdayNames", enUsData, localeData, previous);
+        previous = describeStringArray(result, "tinyWeekdayNames", enUsData, localeData, null);
+        describeStringArray(result, "tinyStandAloneWeekdayNames", enUsData, localeData, previous);
         result.append('\n');
-        
+
         describeString(result, "yesterday", enUsData, localeData);
         describeString(result, "today", enUsData, localeData);
         describeString(result, "tomorrow", enUsData, localeData);
         result.append('\n');
-        
+
         describeChar(result, "zeroDigit", enUsData, localeData);
         describeChar(result, "decimalSeparator", enUsData, localeData);
         describeChar(result, "groupingSeparator", enUsData, localeData);
@@ -210,19 +211,24 @@ public class LocaleActivity extends TextViewActivity {
         describeString(result, "infinity", enUsData, localeData);
         describeString(result, "NaN", enUsData, localeData);
         result.append('\n');
-        
+
       } catch (Exception ex) {
         result.append("(" + ex.getClass().getSimpleName() + " thrown: " + ex.getMessage() + ")\n");
         System.err.println(ex);
       }
     }
-    
+
     return result.toString();
   }
-  
-  private static void describeStringArray(StringBuilder sb, String fieldName, Object enUsData, Object localeData) {
+
+  private static String[] describeStringArray(StringBuilder sb, String fieldName, Object enUsData, Object localeData, String[] previous) {
     try {
       String[] values = (String[]) localeData.getClass().getField(fieldName).get(localeData);
+
+      if (Arrays.equals(values, previous)) {
+        return values;
+      }
+
       String[] enUsValues = (String[]) localeData.getClass().getField(fieldName).get(enUsData);
       sb.append(fieldName).append(":\n");
       for (int i = 0; i < values.length; ++i) {
@@ -235,31 +241,33 @@ public class LocaleActivity extends TextViewActivity {
         }
         sb.append('\n');
       }
+      return values;
     } catch (Exception ignored) {
       sb.append(fieldName).append(": <missing>\n");
+      return null;
     }
   }
-  
+
   private static void describeString(StringBuilder sb, String fieldName, Object enUsData, Object localeData) {
     String valueText = "<missing>";
     try {
       valueText = unicodeString((String) localeData.getClass().getField(fieldName).get(localeData));
     } catch (Exception ignored) {
     }
-    
+
     sb.append(fieldName + ": " + valueText + "\n");
   }
-  
+
   private static void describeChar(StringBuilder sb, String fieldName, Object enUsData, Object localeData) {
     String valueText = "<missing>";
     try {
       valueText = unicodeString(localeData.getClass().getField(fieldName).getChar(localeData));
     } catch (Exception ignored) {
     }
-    
+
     sb.append(fieldName + ": " + valueText + "\n");
   }
-  
+
   private static Object getLocaleDataInstance(Locale locale) {
     try {
       return localeDataClass().getMethod("get", Locale.class).invoke(null, locale);
@@ -267,11 +275,11 @@ public class LocaleActivity extends TextViewActivity {
       throw new RuntimeException(ex);
     }
   }
-  
+
   private static boolean hasLocaleData() {
     return localeDataClass() != null;
   }
-  
+
   private static Class<?> localeDataClass() {
     try {
       return Class.forName("libcore.icu.LocaleData");
@@ -279,11 +287,11 @@ public class LocaleActivity extends TextViewActivity {
       return null;
     }
   }
-  
+
   private static String unicodeString(char ch) {
     return unicodeString(Character.toString(ch));
   }
-  
+
   private static String unicodeString(String s) {
     if (s.length() > 1) {
       // For actual text (like the Arabic NaN), this isn't obviously useful.
@@ -302,7 +310,7 @@ public class LocaleActivity extends TextViewActivity {
     sb.append(")");
     return sb.toString();
   }
-  
+
   private static boolean containsNonAscii(String s) {
     for (int i = 0; i < s.length(); ++i) {
       if (s.charAt(i) > 0x7f) {
@@ -311,7 +319,7 @@ public class LocaleActivity extends TextViewActivity {
     }
     return false;
   }
-  
+
   private static void appendAvailability(StringBuilder result, Locale locale, String name, Class<?> c) {
     String state = "unknown";
     try {
@@ -322,21 +330,21 @@ public class LocaleActivity extends TextViewActivity {
     }
     result.append(name + ": " + state + "\n");
   }
-  
+
   private static void describeDecimalFormatSymbols(StringBuilder result, DecimalFormatSymbols dfs) {
     result.append("Currency Symbol: " + unicodeString(dfs.getCurrencySymbol()) + "\n");
     result.append("International Currency Symbol: " + unicodeString(dfs.getInternationalCurrencySymbol()) + "\n");
     result.append('\n');
-    
+
     result.append("Digit: " + unicodeString(dfs.getDigit()) + "\n");
     result.append("Pattern Separator: " + unicodeString(dfs.getPatternSeparator()) + "\n");
     result.append('\n');
-    
+
     result.append("Decimal Separator: " + unicodeString(dfs.getDecimalSeparator()) + "\n");
     result.append("Monetary Decimal Separator: " + unicodeString(dfs.getMonetaryDecimalSeparator()) + "\n");
     // 1.6: result.append("Exponent Separator: " + dfs.getExponentSeparator() + "\n");
     result.append("Grouping Separator: " + unicodeString(dfs.getGroupingSeparator()) + "\n");
-    
+
     result.append("Infinity: " + unicodeString(dfs.getInfinity()) + "\n");
     result.append("Minus Sign: " + unicodeString(dfs.getMinusSign()) + "\n");
     result.append("NaN: " + unicodeString(dfs.getNaN()) + "\n");
@@ -351,7 +359,7 @@ public class LocaleActivity extends TextViewActivity {
     result.append('\n');
     result.append('\n');
   }
-  
+
   private static void describeDateFormat(StringBuilder result, String description, DateFormat dateFormat, Date when) {
     if (dateFormat instanceof SimpleDateFormat) {
       SimpleDateFormat sdf = (SimpleDateFormat) dateFormat;
@@ -359,7 +367,7 @@ public class LocaleActivity extends TextViewActivity {
       result.append("    " + sdf.format(when) + "\n");
     }
   }
-  
+
   private static void describeNumberFormat(StringBuilder result, String description, NumberFormat numberFormat, Number... values) {
     if (numberFormat instanceof DecimalFormat) {
       DecimalFormat df = (DecimalFormat) numberFormat;
