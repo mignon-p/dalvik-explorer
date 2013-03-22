f3087680c5f7702178940524d33caf2beff5a19d
elliott.n.hughes@gmail.com <elliott.n.hughes@gmail.com@825dafb7-1f56-0410-864d-61665d7775f4>
2013-03-22 04:07:28 +0000

    dalvik-explorer/src/org/jessies/dalvikexplorer/DalvikExplorerActivity.java: add version number to title.
    dalvik-explorer/res/layout/build_widget.xml: make build text smaller so long builds fit.
    
    git-svn-id: http://enh.googlecode.com/svn/trunk@658 825dafb7-1f56-0410-864d-61665d7775f4

--- dalvik-explorer/src/org/jessies/dalvikexplorer/DalvikExplorerActivity.java
+++ dalvik-explorer/src/org/jessies/dalvikexplorer/DalvikExplorerActivity.java
@@ -2,6 +2,7 @@ package org.jessies.dalvikexplorer;
 
 import android.app.*;
 import android.content.*;
+import android.content.pm.*;
 import android.net.*;
 import android.os.*;
 import android.view.*;
@@ -9,37 +10,44 @@ import android.widget.*;
 import java.util.*;
 
 public class DalvikExplorerActivity extends ListActivity {
-    // A (list view label, activity class) pair.
-    private static class NamedActivity {
-        private final String name;
-        private final Class<?> activityClass;
-        private NamedActivity(String name, Class<?> activityClass) {
-            this.name = name;
-            this.activityClass = activityClass;
-        }
-        @Override public String toString() {
-            return name;
-        }
+  // A (list view label, activity class) pair.
+  private static class NamedActivity {
+    private final String name;
+    private final Class<?> activityClass;
+    private NamedActivity(String name, Class<?> activityClass) {
+      this.name = name;
+      this.activityClass = activityClass;
     }
-    
-    private static final NamedActivity[] ACTIVITIES = new NamedActivity[] {
-        new NamedActivity("Build Details", BuildActivity.class),
-        new NamedActivity("Charsets", CharsetsActivity.class),
-        new NamedActivity("Device Details", DeviceActivity.class),
-        new NamedActivity("Environment Variables", EnvironmentVariablesActivity.class),
-        new NamedActivity("File Systems", FileSystemsActivity.class),
-        new NamedActivity("Locales", LocalesActivity.class),
-        new NamedActivity("System Properties", SystemPropertiesActivity.class),
-        new NamedActivity("Time Zones", TimeZonesActivity.class)
-    };
-    
-    @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setListAdapter(new BetterArrayAdapter<NamedActivity>(this, Arrays.asList(ACTIVITIES)));
+    @Override public String toString() {
+      return name;
     }
-    
-    @Override protected void onListItemClick(ListView l, View v, int position, long id) {
-        final NamedActivity destinationActivity = ACTIVITIES[position];
-        startActivity(new Intent(this, destinationActivity.activityClass));
+  }
+
+  private static final NamedActivity[] ACTIVITIES = new NamedActivity[] {
+    new NamedActivity("Build Details", BuildActivity.class),
+    new NamedActivity("Charsets", CharsetsActivity.class),
+    new NamedActivity("Device Details", DeviceActivity.class),
+    new NamedActivity("Environment Variables", EnvironmentVariablesActivity.class),
+    new NamedActivity("File Systems", FileSystemsActivity.class),
+    new NamedActivity("Locales", LocalesActivity.class),
+    new NamedActivity("System Properties", SystemPropertiesActivity.class),
+    new NamedActivity("Time Zones", TimeZonesActivity.class)
+  };
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setListAdapter(new BetterArrayAdapter<NamedActivity>(this, Arrays.asList(ACTIVITIES)));
+
+    String title = "Dalvik Explorer";
+    try {
+      title += " " + getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+    } catch (PackageManager.NameNotFoundException ignored) {
     }
+    setTitle(title);
+  }
+
+  @Override protected void onListItemClick(ListView l, View v, int position, long id) {
+    final NamedActivity destinationActivity = ACTIVITIES[position];
+    startActivity(new Intent(this, destinationActivity.activityClass));
+  }
 }
